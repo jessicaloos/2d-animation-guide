a568ba7045c602e86a5599342cb2e91dacecb5a6
Jessica <113109265+jessicaloos@users.noreply.github.com>
2025-04-14 18:49:02 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-function switchLang(lang) {
-    const allSections = document.querySelectorAll('.lang');
-    allSections.forEach(section => {
-        section.style.display = section.classList.contains('lang-' + lang) ? 'block' : 'none';
-    });
-}
